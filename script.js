083db27282b41039c547d68eef7ddebfb50deb5c
Ryan Soeder <mr.soeder@gmail.com>
2020-06-21 13:37:13 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// FIX FOR VH UNITS IN SAFARI
+document.querySelector('header').style.height = window.innerHeight + 'px';
+document.querySelector('.listen').style.height = window.innerHeight + 'px';
