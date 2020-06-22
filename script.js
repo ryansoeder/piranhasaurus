af422f94027f34d69e62e22a402f8366c79aa9e7
Ryan Soeder <mr.soeder@gmail.com>
2020-06-22 15:44:16 -0400

    navigation menu

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // FIX FOR VH UNITS IN SAFARI
-document.querySelector('header').style.height = window.innerHeight + 'px';
-document.querySelector('.listen').style.height = window.innerHeight + 'px';
+// document.querySelector('header').style.height = window.innerHeight + 'px';
+// document.querySelector('.listen').style.height = window.innerHeight + 'px';
+
+const menuToggle = document.querySelector('#menuToggle');
+const menu = document.querySelector('#menu');
+let menuOpen = false;
+menuToggle.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuToggle.classList.add('open');
+        menu.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuToggle.classList.remove('open');
+        menu.classList.remove('open');
+        menuOpen = false;
+    }
+})
