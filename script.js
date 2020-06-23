59db750162352d6e4502467d4fbd1b64b039288b
Ryan Soeder <mr.soeder@gmail.com>
2020-06-23 17:49:55 -0400

    nav menu overhaul

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 // document.querySelector('header').style.height = window.innerHeight + 'px';
 // document.querySelector('.listen').style.height = window.innerHeight + 'px';
 
-const menuToggle = document.querySelector('#menuToggle');
+const menuBtn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#menu');
 let menuOpen = false;
-menuToggle.addEventListener('click', () => {
+menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
-        menuToggle.classList.add('open');
+        menuBtn.classList.add('open');
         menu.classList.add('open');
         menuOpen = true;
     } else {
-        menuToggle.classList.remove('open');
+        menuBtn.classList.remove('open');
         menu.classList.remove('open');
         menuOpen = false;
     }
