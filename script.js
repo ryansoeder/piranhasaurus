2dd97c94be648845990b8a7167bd9d05d1fb0fa2
Ryan Soeder <mr.soeder@gmail.com>
2020-06-24 14:39:13 -0400

    translateZ effects to nav menu li and delay timing to nav menu animations

--- script.js
+++ script.js
@@ -4,15 +4,24 @@
 
 const menuBtn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#menu');
+const menuItems = document.querySelectorAll('#menu li');
 let menuOpen = false;
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menu.classList.add('open');
+
+        menuItems.forEach(item => {item.classList.add('open');});
+
+
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menu.classList.remove('open');
+
+        menuItems.forEach(item => {item.classList.remove('open');});
+
         menuOpen = false;
     }
 })
